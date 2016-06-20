1e20848f550d86da17c110e1076eb7e1c5ad642f
camerongrundy <cgrundy@purdue.edu>
2016-06-20 15:41:48 -0400

    Finished up mutants.html

--- app/config.route.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('mutantApp.home')
-    .config(configFunction);
-
-    configFunction.$inject = [];
-})();
---
+++ app/mutantList/mutantList.controller.js
@@ -0,0 +1,28 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mutantApp.mutantList')
+    .controller('MutantListController', MutantListController);
+
+  function MutantListController() {
+    var vm = this;
+
+    vm.addMutant = addMutant;
+    vm.mutants = ['deadpool', 'nightcrawler', 'gambit'];
+    vm.newMutant = new Mutant();
+
+
+    function Mutant() {
+      this.name = '';
+      this.phone = '';
+      this.topic = '';
+      this.notified = false;
+      this.complete = false;
+    }
+
+    function addMutant() {
+      vm.mutants.push(vm.newMutant);
+    }
+  }
+})();
